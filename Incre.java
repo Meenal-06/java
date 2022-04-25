e8e98483ffd2f3350fe81d5ed9dbccc98143321e
Meenal-06 <96614365+Meenal-06@users.noreply.github.com>
2022-04-25 16:31:05 +0530

    increment and decrement

---
+++ Incre.java
@@ -0,0 +1,14 @@
+class Incre{
+	public static void main(String args[])
+	{
+	 int a=10,b,c,d;
+	 ++a;
+	 b=a++;
+	 c=a--;
+	 d=--a;
+	 System.out.println(a);
+	 System.out.println(b);
+	 System.out.println(c);
+ 	 System.out.println(d);
+	}
+}
