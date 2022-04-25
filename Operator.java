a78d21b32a2fc986c7aafb12bf8329012d739cdc
Meenal-06 <96614365+Meenal-06@users.noreply.github.com>
2022-04-25 16:37:44 +0530

    left and right shift

---
+++ Operator.java
@@ -0,0 +1,13 @@
+class Operator{
+	public static void main(String args[])
+	{
+		int a=8<<2;
+		int b=-10<<2;
+		int c=11>>2;
+		int d=-18>>2;
+		System.out.println("Value of 8 after left shifted 2= "+a);
+		System.out.println("Value of -10 after left shifted 2= "+b);
+		System.out.println("Value of 11 after right shifted 2= "+c);
+		System.out.println("value of -18 after right shifted by 2= "+d);
+	}
+}
