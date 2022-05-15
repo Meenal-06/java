3cc2fc7a11d4c13b6eb2c8e0deeee605eb2b1665
Meenal-06 <96614365+Meenal-06@users.noreply.github.com>
2022-05-15 20:04:24 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,12 @@
+class Array{
+public static void main(String args[])
+{
+int a[]={10,20,30,40,50};
+for(int i:a)
+//for( int i=0;i<a.length-1;i++)
+{
+//System.out.println(a[i]);
+System.out.println(i);
+}
+}
+}
