e0a3a35cefbb87688969e5326897a342be44719e
Meenal-06 <96614365+Meenal-06@users.noreply.github.com>
2022-05-18 18:48:35 +0530

    String class

---
+++ Str.java
@@ -0,0 +1,10 @@
+class Str
+{
+public static void main(String args[])
+{
+String s1=new String("Hello");
+System.out.println("String = "+s1);
+System.out.println("String hashcode = "+s1.hashCode());
+System.out.println("Length of string ="+s1.length());
+}
+}
