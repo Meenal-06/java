f62cabc786ec54c93e1d7174a78db647a23de4db
Meenal-06 <96614365+Meenal-06@users.noreply.github.com>
2022-05-20 11:44:07 +0530

    equalignorecasemethod

---
+++ Equalignorecase.java
@@ -0,0 +1,11 @@
+class Equalignorecase{
+public static void main(String args[])
+{
+String str1=new String ("Hello");
+String str2=new String ("hello");
+if(str1.equalsIgnoreCase(str2))
+System.out.println("Same value");
+else
+System.out.println("Different value");
+}
+}
