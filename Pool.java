1c3233a19ec610d5dc74823c0413497df0c4eab3
Meenal-06 <96614365+Meenal-06@users.noreply.github.com>
2022-05-20 11:42:49 +0530

    pool memory concept

---
+++ Pool.java
@@ -0,0 +1,11 @@
+class Pool{
+public static void main(String args[])
+{
+String str1=new String ("Hello");
+String str2=new String ("Hello");
+if(str1==str2)
+System.out.println("Same value");
+else
+System.out.println("Different value");//because opertor work on Physical add.and physical add of str1,str2 are different 
+}
+}
