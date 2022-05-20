f933ecf37d95c87d2d29060f060f447e133eb14b
Meenal-06 <96614365+Meenal-06@users.noreply.github.com>
2022-05-20 11:45:33 +0530

    Comparetoignorecase method

---
+++ comparetoignore.java
@@ -0,0 +1,9 @@
+class Comparetoignore{
+public static void main(String args[])
+{
+String str1=new String ("Hello");
+String str2=new String ("hello World");
+int s =(str1.compareToIgnoreCase(str2));
+System.out.println(s);
+}
+}
