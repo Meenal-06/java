68dbecabd94cf478f8a01e726212854dd8d82930
Meenal-06 <96614365+Meenal-06@users.noreply.github.com>
2022-05-20 11:44:58 +0530

    compareto method

---
+++ Comapreto.java
@@ -0,0 +1,9 @@
+class Compareto{
+public static void main(String args[])
+{
+String str1=new String ("Hello");
+String str2=new String ("hello");
+int s =(str1.compareTo(str2));
+System.out.println(s);
+}
+}
