a9065da9a398fc3d0c357b58f4528e0b744dd5d6
Meenal-06 <96614365+Meenal-06@users.noreply.github.com>
2022-05-22 17:15:35 +0530

    indexof method

---
+++ Indexof.java
@@ -0,0 +1,13 @@
+class Indexof{
+public static void main(String arg[])
+{
+String str=new String("india");
+String str1="indore is in india";
+System.out.println(str.indexOf('i'));
+System.out.println(str.indexOf('i',1));
+System.out.println(str1.indexOf("in"));
+System.out.println(str1.indexOf("in",1));
+}
+}
+
+
