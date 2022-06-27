f5f1227c49c28e7d04bb9c9a6ce45f718b3943c6
Meenal-06 <96614365+Meenal-06@users.noreply.github.com>
2022-06-27 19:03:21 +0530

    throw in exception handling

---
+++ Throw.java
@@ -0,0 +1,22 @@
+class Throw{
+public static void main(String args[]){
+int a=10,b=1,c;
+try
+{
+if(b==1)
+{ 
+throw new ArithmeticException("/ by one");
+}
+else
+{
+c=a/b;
+System.out.println(c);
+}
+}
+catch(ArithmeticException e)
+{
+ System.out.println(e);
+}
+System.out.println("Bye");
+}
+}
