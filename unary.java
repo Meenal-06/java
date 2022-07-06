42aa2c085ed060bf8341e7bf52508b2f1e95e376
Meenal-06 <96614365+Meenal-06@users.noreply.github.com>
2022-07-06 22:43:59 +0530

    unary operators

---
+++ unary.java
@@ -0,0 +1,15 @@
+class Unary{
+public static void main(String args[])
+{
+int a=10,b,c,d;
+//b=a++;
+//c=++b;
+//d=c++;
+//a=++d;
+b=++a;
+c=b++;
+d=++c;
+a=d++;
+System.out.println(a+" "+b+" "+c+" "+d);
+}
+}
