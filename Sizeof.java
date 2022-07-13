bd4e6359155ede40947bc889a688005f48d5411a
Meenal-06 <96614365+Meenal-06@users.noreply.github.com>
2022-07-13 20:45:48 +0530

    Size and range of datatypes

---
+++ Sizeof.java
@@ -0,0 +1,21 @@
+class Sizeof{
+public static void main(String args[])
+{
+System.out.println("Size of Byte: "+(Byte.SIZE/8)+" bytes");
+System.out.println("Size of short : "+(Short.SIZE/8)+" bytes");
+System.out.println("Size of int : "+(Integer.SIZE/8)+" bytes");
+System.out.println("Size of long : "+(Long.SIZE/8)+" bytes");
+System.out.println("Size of float: "+(Float.SIZE/8)+" bytes");
+System.out.println("Size of double: "+(Double.SIZE/8)+" bytes");
+System.out.println("Size of char: "+(Character.SIZE/8)+" bytes");
+System.out.println("****************************************************");
+System.out.println("Rnage of byte"+(Byte.MIN_VALUE)+" - "+(Byte.MAX_VALUE));
+System.out.println("Rnage of short"+(Short.MIN_VALUE)+" - "+(Short.MAX_VALUE));
+System.out.println("Rnage of int"+(Integer.MIN_VALUE)+" - "+(Integer.MAX_VALUE));
+System.out.println("Rnage of long"+(Long.MIN_VALUE)+" - "+(Long.MAX_VALUE));
+System.out.println("Rnage of float"+(Float.MIN_VALUE)+" - "+(Float.MAX_VALUE));
+System.out.println("Rnage of double"+(Double.MIN_VALUE)+" - "+(Double.MAX_VALUE));
+System.out.println("Rnage of char"+(Character.MIN_VALUE)+" - "+(Character.MAX_VALUE));
+
+}
+}
